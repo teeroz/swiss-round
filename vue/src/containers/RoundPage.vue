5628c07812172cb8fc537f4235dd8c2525379310
Alvin <alvin@radishfiction.com>
2022-09-23 00:28:35 +0900

    vue version up

--- vue/src/containers/RoundPage.vue
+++ vue/src/containers/RoundPage.vue
@@ -23,19 +23,19 @@
           <div class="d-flex justify-content-center">
             <div class="col-6 text-right">
               <span @click="goPlayerPage(match.player1)">
-                <template v-if="league.win_mode == 'half'"><small>{{ match.player1.score }}점</small> {{ match.player1.name }} </template>
+                <template v-if="league.win_mode === 'half'"><small>{{ match.player1.score }}점</small> {{ match.player1.name }} </template>
                 <template v-else><small>{{ match.player1.wins }}승</small> {{ match.player1.name }} </template>
               </span>
 
-              <template v-if="league.win_mode == 'half'">
+              <template v-if="league.win_mode === 'half'">
                 <button 
                   type="button" 
                   class="btn btn-sm ml-1"
-                  :class="{'btn-secondary': match.score1 == match.score2, 'btn-primary': match.score1 == 2, 'btn-outline-primary': match.score1 == 1, 'btn-danger': match.score2 == 2, 'btn-outline-danger': match.score2 == 1}"
-                  @click="updateScore(match, match.score2 == 0 ? Math.min(match.score1 + 1, 2) : 0, match.score2 == 0 ? 0 : match.score2 - 1)"
+                  :class="{'btn-secondary': match.score1 === match.score2, 'btn-primary': match.score1 === 2, 'btn-outline-primary': match.score1 === 1, 'btn-danger': match.score2 === 2, 'btn-outline-danger': match.score2 === 1}"
+                  @click="updateScore(match, match.score2 === 0 ? Math.min(match.score1 + 1, 2) : 0, match.score2 === 0 ? 0 : match.score2 - 1)"
                   >
-                  <template v-if="match.score1 > match.score2">{{ match.score1 == 1 ? '½' : '' }}승</template>
-                  <template v-else-if="match.score1 < match.score2">{{ match.score2 == 1 ? '½' : '' }}패</template>
+                  <template v-if="match.score1 > match.score2">{{ match.score1 === 1 ? '½' : '' }}승</template>
+                  <template v-else-if="match.score1 < match.score2">{{ match.score2 === 1 ? '½' : '' }}패</template>
                   <template v-else>무</template>
                 </button>
               </template>
@@ -43,7 +43,7 @@
                 <button 
                   type="button" 
                   class="btn btn-sm ml-1"
-                  :class="{'btn-secondary': match.score1 == match.score2, 'btn-primary': match.score1 > match.score2, 'btn-danger': match.score1 < match.score2}"
+                  :class="{'btn-secondary': match.score1 === match.score2, 'btn-primary': match.score1 > match.score2, 'btn-danger': match.score1 < match.score2}"
                   @click="updateScore(match, match.score1 ? 0 : 1, 0)"
                   >
                   <template v-if="match.score1 > match.score2">승</template>
@@ -54,15 +54,15 @@
             </div>
             <div><small>#{{ index + 1 }}</small></div>
             <div class="col-6 text-left">
-              <template v-if="league.win_mode == 'half'">
+              <template v-if="league.win_mode === 'half'">
                 <button 
                   type="button" 
                   class="btn btn-sm ml-1"
-                  :class="{'btn-secondary': match.score1 == match.score2, 'btn-primary': match.score2 == 2, 'btn-outline-primary': match.score2 == 1, 'btn-danger': match.score1 == 2, 'btn-outline-danger': match.score1 == 1}"
-                  @click="updateScore(match, match.score1 == 0 ? 0 : match.score1 - 1, match.score1 == 0 ? Math.min(match.score2 + 1, 2) : 0)"
+                  :class="{'btn-secondary': match.score1 === match.score2, 'btn-primary': match.score2 === 2, 'btn-outline-primary': match.score2 === 1, 'btn-danger': match.score1 === 2, 'btn-outline-danger': match.score1 === 1}"
+                  @click="updateScore(match, match.score1 === 0 ? 0 : match.score1 - 1, match.score1 === 0 ? Math.min(match.score2 + 1, 2) : 0)"
                   >
-                  <template v-if="match.score2 > match.score1">{{ match.score2 == 1 ? '½' : '' }}승</template>
-                  <template v-else-if="match.score2 < match.score1">{{ match.score1 == 1 ? '½' : '' }}패</template>
+                  <template v-if="match.score2 > match.score1">{{ match.score2 === 1 ? '½' : '' }}승</template>
+                  <template v-else-if="match.score2 < match.score1">{{ match.score1 === 1 ? '½' : '' }}패</template>
                   <template v-else>무</template>
                 </button>
               </template>
@@ -70,7 +70,7 @@
                 <button 
                   type="button" 
                   class="btn btn-sm mr-1"
-                  :class="{'btn-secondary': match.score2 == match.score1, 'btn-primary': match.score2 > match.score1, 'btn-danger': match.score2 < match.score1}"
+                  :class="{'btn-secondary': match.score2 === match.score1, 'btn-primary': match.score2 > match.score1, 'btn-danger': match.score2 < match.score1}"
                   @click="updateScore(match, 0, match.score2 ? 0 : 1)"
                   >
                   <template v-if="match.score2 > match.score1">승</template>
@@ -80,7 +80,7 @@
               </template>
 
               <span @click="goPlayerPage(match.player2)">
-                <template v-if="league.win_mode == 'half'"> {{ match.player2.name }} <small>{{ match.player2.score }}점</small></template>
+                <template v-if="league.win_mode === 'half'"> {{ match.player2.name }} <small>{{ match.player2.score }}점</small></template>
                 <template v-else> {{ match.player2.name }} <small>{{ match.player2.wins }}승</small></template>
               </span>
             </div>
@@ -137,6 +137,8 @@
 </template>
 
 <script>
+import _ from 'lodash';
+
 export default {
   name: 'RoundPage',
 
@@ -169,8 +171,8 @@ export default {
         this.$refs.loading.stop()
 
         // 테이블별로 2개씩 청크로 만들어서 저장한다
-        const matches1 = matches.slice(0, parseInt((matches.length + 1) / 2, 10));
-        const matches2 = matches.slice(parseInt((matches.length + 1) / 2, 10));
+        const matches1 = matches.slice(0, Math.floor((matches.length + 1) / 2));
+        const matches2 = matches.slice(Math.floor((matches.length + 1) / 2));
         const matchesByTable = [];
         // eslint-disable-next-line
         for (const k in matches1) {
@@ -179,7 +181,7 @@ export default {
             matchesByTable.push(matches2[k]);
           }
         }
-        this.matchesByTable = this._.chunk(matchesByTable, 2);
+        this.matchesByTable = _.chunk(matchesByTable, 2);
       })
   },
 
